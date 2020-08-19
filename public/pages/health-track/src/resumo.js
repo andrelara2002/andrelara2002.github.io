dd0d396548e26442e97be267b4ade9008a93757e
andrelara2002 <andrelara2002@gmail.com>
2020-08-18 22:36:28 -0300

    Adicionando calculo de imc

--- public/pages/health-track/src/resumo.js
+++ public/pages/health-track/src/resumo.js
@@ -1,6 +1,8 @@
 const alturaElement = document.getElementById('valorAltura');
 const passosElement = document.getElementById('valorPassos');
 const pesoElement = document.getElementById('valorPeso')
+const imcElement = document.getElementById('valorIMC')
+const tagElement = document.getElementById('tagIMC')
 
 var pesoValue = JSON.parse(localStorage.getItem('lista_peso')) || []
 var alturaValue = JSON.parse(localStorage.getItem('lista_altura')) || []
@@ -26,9 +28,38 @@ function render(){
 
     textPeso = document.createTextNode(`${userStats.peso} Kg`);
     pesoElement.appendChild(textPeso);
+
+    calcularIMC()
 }
 document.getElementById('card-button').onclick = () =>{
     window.location.href = 'userEsportes.html'
+}
+
+calcularIMC = () =>{
+    const {passos, altura, peso} = userStats
+    let result = '';
+
+    imcElement.innerHTML = ''
+    let valorIMC = Math.round(peso/(altura*altura));
+
+    if (valorIMC < 18.5){
+        result = 'Muito Magro'
+    }
+    else if(valorIMC >= 18.5 && valorIMC <= 24.9){
+        result = 'Peso normal'
+    }
+    else if (valorIMC >=25 && valorIMC <= 29.9){
+        result = 'Sobrepeso'
+    }
+    else if (valorIMC >=30 && valorIMC <= 39.9){
+        result = 'Obesidade'
+    }
+    else if (valorIMC >= 40){
+        result = 'Obesidade Grave'
+    }
+
+    imcElement.innerHTML = `IMC ${valorIMC}`
+    tagElement.innerHTML = result
 }
 
 goToPeso = () =>{
